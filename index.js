9541deb66007b4323d959c952bc8063cca226a02
Rayan Dutta <rondutta23@gmail.com>
2018-05-14 11:17:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
  else if(string.toLowerCase()===string){
    return  "I can't hear you!";
  }
- else if(string === "I love you, Grandma")
+ else if(string === "I love you, Grandma.")
    return  "I love you, too.";
 }
 
