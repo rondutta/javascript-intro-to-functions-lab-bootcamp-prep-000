9359a775c119a45e99944c59294c9bd30ac31ead
Rayan Dutta <rondutta23@gmail.com>
2018-05-14 11:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,16 @@ function logWhisper (string) {
   console.log(string.toLowerCase());
 }
 
+
 function sayHiToGrandma(string){
-  var message = null;
  if(string.toUpperCase()===string){
    message = "YES INDEED!";
  }
  else if(string.toLowerCase()===string){
-   message =  "I can't hear you!";
+   return  "I can't hear you!";
  }
- else if(string === "I love you, Grandma"){
-   message =  "I love you, too.";
- }
- return message;
+ else if(string === "I love you, Grandma")
+   return  "I love you, too.";
 }
 
 
